b0e97166011792fb515e9e0bd7acc677072dac7d
Javier Enrique Lopez Beltran <javierelb1205@gmail.com>
2025-03-05 09:46:33 -0500

    Jota1205.py

---
+++ Roadmap/00 - SINTAXIS, VARIABLES, TIPOS DE DATOS Y HOLA MUNDO/python/Jota1205.py
@@ -0,0 +1,14 @@
+# https://python.org/
+
+# Comentario en una linea
+"""
+Comentario
+en
+varias
+líneas jaja
+"""
+
+my_variable = "mi variable"
+
+MY_CONSTANT = "Mi constante" # por convención
+MY_CONSTANT = "ASDASDASDASSA"
